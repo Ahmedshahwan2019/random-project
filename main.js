37f5ae0474bd2d8d1e0a259f72361d00224058d4
ahmed shahwan <shahwanahmed15@outlook.com>
2020-09-08 01:05:17 +0200

    modify html ad js flies

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-alert('hi there');
+const price = document.querySelector('[name=price]');
+const quantity = document.querySelector('[name=quantity]');
+const totalCost = document.getElementById('totalCost');
+const labelSpan = document.getElementById('label-span');
+console.log(labelSpan);
+
+const calcTotal = () => {
+  const prices = price.value;
+  const quantities = quantity.value;
+  const total = prices * quantities;
+  totalCost.innerText = '$ ' + total.toFixed(2);
+};
+calcTotal();
+
+function updateQuantity() {
+  let quantities = quantity.value;
+  labelSpan.innerText = quantities;
+}
+updateQuantity();
+price.addEventListener('input', calcTotal);
+quantity.addEventListener('input', calcTotal);
+labelSpan.addEventListener('input', updateQuantity);
